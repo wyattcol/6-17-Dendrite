2df15aaae86a1db8ac02159cbe194f2d7dcfdd03
wyattcol <35353860+wyattcol@users.noreply.github.com>
2019-06-20 14:58:17 -0400

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -3,5 +3,6 @@ del Dendrite.dll
 del Version.txt
 ren "DendriteUpdated.exe" "Dendrite.exe"	
 ren "DendriteUpdated.dll" "Dendrite.dll"	
+ren "NewVersion.txt" "Version.txt"
 start Dendrite.exe
 del Update.bat
